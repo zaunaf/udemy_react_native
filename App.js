2d9884b43145f83b1782f507a2e0784bfcd1ab42
Abah Z <zaunaf@yahoo.com>
2019-10-24 12:53:20 +0700

    Step 2 - Base Components

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, TextInput, Text, View, Button } from 'react-native';
 
 export default function App() {
   const [outputText, setOutputText] = useState("Open up App.js to start working on your app!");
   return (
-    <View style={styles.container}>
-      <Text>{ outputText }</Text>
-      <Button title="Change Text" onPress={() => setOutputText("The text")}/>
+    <View style={{padding: 30}}>
+      <View style={{flexDirection: 'row', justifyContent:'space-between'}}>
+        <TextInput 
+            placeholder="Course goal" 
+            style={{width: '85%', borderBottomColor: 'black', borderBottomWidth: 1, marginBottom:10}}/>
+        <Button title="Add" />        
+      </View>
     </View>
   );
 }
