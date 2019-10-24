bf15dff332444abfa179b20d4c7c6265d717071a
Abah Z <zaunaf@yahoo.com>
2019-10-24 13:12:52 +0700

    Step 6 - Inline Stylesheet vs Stylesheet Object

--- App.js
+++ App.js
@@ -4,33 +4,30 @@ import { StyleSheet, TextInput, Text, View, Button } from 'react-native';
 export default function App() {
   const [outputText, setOutputText] = useState("Open up App.js to start working on your app!");
   return (
-    <View style={{padding: 30}}>
-      <View style={{flexDirection: 'row', justifyContent:'space-between'}}>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
         <TextInput 
             placeholder="Course goal" 
-            style={{width: '85%', borderBottomColor: 'black', borderBottomWidth: 1, marginBottom:10}}/>
+            style={styles.input}/>
         <Button title="Add" />        
-      </View>
-      <View style={{flexDirection:"row", width: "80%", height:300, justifyContent:'space-between', alignItems: "stretch"}}>
-        <View style={{backgroundColor: 'red', flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text>1</Text>
-        </View>
-        <View style={{backgroundColor: 'green', flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text>2</Text>
-        </View>
-        <View style={{backgroundColor: 'blue', flex: 2, justifyContent: 'center', alignItems: 'center'}}>
-            <Text>3</Text>
-        </View>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
+  inputContainer: {
+    flexDirection: 'row', 
+    justifyContent:'space-between'
+  },
+  input: {
+    width: '85%',
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    marginBottom:10
+  },
+  
 });
