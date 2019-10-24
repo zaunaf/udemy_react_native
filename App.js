3aeb51af14f2e61d3a354345108a7eca67d46d41
Abah Z <zaunaf@yahoo.com>
2019-10-24 14:01:21 +0700

    Step 8 - States and Events : Adding List

--- App.js
+++ App.js
@@ -2,13 +2,24 @@ import React, { useState } from 'react';
 import { StyleSheet, TextInput, Text, View, Button } from 'react-native';
 
 export default function App() {
+  // State of enteredGoal (inputted text)
   const [enteredGoal, setEnteredGoal] = useState("");
+  
+  // State of courseGoals list
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  // Menangkap setiap perubahan pada TextInput, menempelkannya pada state
   const goalInputHandler = (enteredText) => {
       setEnteredGoal(enteredText);
   }
+
+  // Menangkap aksi Add dan menambahkan isi TextInput ke list courseGoals
   const addGoalHandler = () => {
-    console.log(enteredGoal);
+    // console.log(enteredGoal);
+    // setCourseGoals([...courseGoals, enteredGoal]);
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
   }
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
@@ -19,6 +30,9 @@ export default function App() {
             value={enteredGoal}
         />
         <Button title="Add" onPress={addGoalHandler}/>
+      </View>
+      <View>
+        {courseGoals.map((goal) => <Text key={goal}>{goal}</Text>)}
       </View>
     </View>
   );
